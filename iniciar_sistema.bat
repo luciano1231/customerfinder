59e559dc7034f6a967024b85d3d1629cb14fae70
Luciano <luciano1231@hotmail.com>
2026-05-13 10:50:08 -0300

    Agrega seccion plantillas y espera aleatoria para evitar bloqueo de Whatsapp

---
+++ iniciar_sistema.bat
@@ -0,0 +1,25 @@
+@echo off
+color 0b
+title Prospector Pro - Panel Central
+
+echo ========================================================
+echo     Iniciando el Motor de Prospector Pro (Servidor)
+echo ========================================================
+:: Abre el servidor en una nueva ventana oculta o paralela
+start "Servidor Local" cmd /c "python server.py"
+
+timeout /t 3 /nobreak > nul
+
+echo.
+echo ========================================================
+echo     Generando tu Enlace Remoto para el Celular...
+echo ========================================================
+echo.
+echo Espera unos segundos y copia el enlace web que aparecera a continuacion
+echo (El que termina en pinggy.link).
+echo Cuando quieras terminar de usarlo, solo cierra esta ventana.
+echo.
+
+ssh -p 443 -o StrictHostKeyChecking=no -R0:localhost:5000 a.pinggy.io
+
+pause
